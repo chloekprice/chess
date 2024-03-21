bd6c1ce30a6f5f81b328bf5334029416e6bf4358
Chloe Price <ckp34@byu.edu>
2024-03-20 23:11:25 -0600

    got one chess POV working

--- client/src/main/java/ui/repls/GamePlayUI.java
+++ client/src/main/java/ui/repls/GamePlayUI.java
@@ -16,15 +16,20 @@ public class GamePlayUI {
 
     }
     public void printBoard() {
-        System.out.println(whitePOV());
+        System.out.print(whitePOV());
+        System.out.println(RESET_BG_COLOR);
         System.out.print(SET_BG_COLOR_BLACK);
-        System.out.println("                            \n");
+        System.out.println("                              ");
+        System.out.println(RESET_BG_COLOR);
         System.out.println(blackPOV());
     }
     private String whitePOV() {
         StringBuilder chessBoard = new StringBuilder();
         chessBoard.append(SET_BG_COLOR_MAGENTA);
-        chessBoard.append("    a  b  c  d  e  f  g  h   \n");
+        chessBoard.append("    a  b  c  d  e  f  g  h    ");
+        chessBoard.append(RESET_BG_COLOR);
+        chessBoard.append("\n");
+        chessBoard.append(SET_BG_COLOR_MAGENTA);
         chessBoard.append(" 1 ");
         chessBoard.append(SET_BG_COLOR_DARK_GREY);
         chessBoard.append(" R ");
@@ -43,19 +48,24 @@ public class GamePlayUI {
         chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
         chessBoard.append(" R ");
         chessBoard.append(SET_BG_COLOR_MAGENTA);
-        chessBoard.append(" 1 \n");
+        chessBoard.append(" 1 ");
+        chessBoard.append(RESET_BG_COLOR);
+        chessBoard.append("\n");
+        chessBoard.append(SET_BG_COLOR_MAGENTA);
         chessBoard.append(" 2 ");
         for (int i = 0; i < 8; i++) {
             if ((i|1) > i) {
                 chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
-                chessBoard.append(" p ");
+                chessBoard.append(" P ");
             } else {
                 chessBoard.append(SET_BG_COLOR_DARK_GREY);
-                chessBoard.append(" p ");
+                chessBoard.append(" P ");
             }
         }
         chessBoard.append(SET_BG_COLOR_MAGENTA);
-        chessBoard.append(" 2 \n");
+        chessBoard.append(" 2 ");
+        chessBoard.append(RESET_BG_COLOR);
+        chessBoard.append("\n");
         for (int i = 0; i < 4; i++) {
             chessBoard.append(SET_BG_COLOR_MAGENTA);
             chessBoard.append(" ");
@@ -64,7 +74,7 @@ public class GamePlayUI {
 
             if ((i|1) > i) {
                 for (int j = 0; j < 8; j++) {
-                    if ((j|1) > i) {
+                    if ((j|1) > j) {
                         chessBoard.append(SET_BG_COLOR_DARK_GREY);
                         chessBoard.append("   ");
                     } else {
@@ -74,7 +84,7 @@ public class GamePlayUI {
                 }
             } else {
                 for (int k = 0; k < 8; k++) {
-                    if ((k|1) > i) {
+                    if ((k|1) > k) {
                         chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
                         chessBoard.append("   ");
                     } else {
@@ -87,17 +97,66 @@ public class GamePlayUI {
             chessBoard.append(SET_BG_COLOR_MAGENTA);
             chessBoard.append(" ");
             chessBoard.append((i+3));
-            chessBoard.append(" \n");
+            chessBoard.append(" ");
+            chessBoard.append(RESET_BG_COLOR);
+            chessBoard.append("\n");
+
         }
-        chessBoard.append("    a  b  c  d  e  f  g  h   ");
+        chessBoard.append(SET_BG_COLOR_MAGENTA);
+        chessBoard.append(" 7 ");
+        chessBoard.append(SET_TEXT_COLOR_BLACK);
+        for (int i = 0; i < 8; i++) {
+            if ((i|1) > i) {
+                chessBoard.append(SET_BG_COLOR_DARK_GREY);
+                chessBoard.append(" p ");
+            } else {
+                chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
+                chessBoard.append(" p ");
+            }
+        }
+        chessBoard.append(SET_BG_COLOR_MAGENTA);
+        chessBoard.append(SET_TEXT_COLOR_WHITE);
+        chessBoard.append(" 7 ");
+        chessBoard.append(RESET_BG_COLOR);
+        chessBoard.append("\n");
+        chessBoard.append(SET_BG_COLOR_MAGENTA);
+        chessBoard.append(" 8 ");
+        chessBoard.append(SET_TEXT_COLOR_BLACK);
+        chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
+        chessBoard.append(" R ");
+        chessBoard.append(SET_BG_COLOR_DARK_GREY);
+        chessBoard.append(" N ");
+        chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
+        chessBoard.append(" B ");
+        chessBoard.append(SET_BG_COLOR_DARK_GREY);
+        chessBoard.append(" K ");
+        chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
+        chessBoard.append(" Q ");
+        chessBoard.append(SET_BG_COLOR_DARK_GREY);
+        chessBoard.append(" B ");
+        chessBoard.append(SET_BG_COLOR_LIGHT_GREY);
+        chessBoard.append(" N ");
+        chessBoard.append(SET_BG_COLOR_DARK_GREY);
+        chessBoard.append(" R ");
+        chessBoard.append(SET_BG_COLOR_MAGENTA);
+        chessBoard.append(SET_TEXT_COLOR_WHITE);
+        chessBoard.append(" 8 ");
+        chessBoard.append(RESET_BG_COLOR);
+        chessBoard.append("\n");
+        chessBoard.append(SET_BG_COLOR_MAGENTA);
+        chessBoard.append("    a  b  c  d  e  f  g  h    ");
         return String.valueOf(chessBoard);
     }
     private String blackPOV() {
         StringBuilder chessBoard = new StringBuilder();
         chessBoard.append(SET_BG_COLOR_MAGENTA);
-        chessBoard.append("   h  g  f  e  d  c  b  a   \n");
+        chessBoard.append("   h  g  f  e  d  c  b  a    ");
+        chessBoard.append(RESET_BG_COLOR);
+        chessBoard.append("\n");
         chessBoard.append(SET_BG_COLOR_MAGENTA);
-        chessBoard.append("   h  g  f  e  d  c  b  a   \n");
+        chessBoard.append("   h  g  f  e  d  c  b  a    ");
+        chessBoard.append(RESET_BG_COLOR);
+        chessBoard.append("\n");
         return String.valueOf(chessBoard);
     }
 }
