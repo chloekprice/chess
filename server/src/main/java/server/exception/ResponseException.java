45d543b5d0316c5d0b8462891c818fa30de4c150
Chloe Price <ckp34@byu.edu>
2024-04-04 20:36:35 -0600

    implemented websocket stuff in the server

---
+++ server/src/main/java/server/exception/ResponseException.java
@@ -0,0 +1,20 @@
+package server.exception;
+
+public class ResponseException extends Exception {
+    final private int statusCode;
+    final private String message;
+
+    public ResponseException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public int StatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ server/src/main/java/server/websockets/WebSocketHandler.java
@@ -0,0 +1,53 @@
+package server.websockets;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.springframework.security.core.userdetails.User;
+import server.exception.ResponseException;
+import webSocketMessages.userCommands.UserGameCommand;
+import webSocketMessages.serverMessages.ServerMessage;
+
+import java.io.IOException;
+import java.util.Timer;
+
+
+@WebSocket
+public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case UserGameCommand.CommandType.JOIN_PLAYER -> enter(command.getAuthString(), session);
+            case UserGameCommand.CommandType.LEAVE -> exit(command.getAuthString());
+        }
+    }
+
+    private void enter(String visitorName, Session session) throws IOException {
+        connections.add(visitorName, session);
+        var message = String.format("%s is in the shop", visitorName);
+        var notification = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
+        connections.broadcast(visitorName, notification);
+    }
+
+    private void exit(String visitorName) throws IOException {
+        connections.remove(visitorName);
+        var message = String.format("%s left the shop", visitorName);
+        var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        connections.broadcast(visitorName, notification);
+    }
+
+    public void makeNoise(String petName, String sound) throws ResponseException {
+        try {
+            var message = String.format("%s says %s", petName, sound);
+            var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+            connections.broadcast("", notification);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+}
